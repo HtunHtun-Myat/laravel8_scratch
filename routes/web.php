c9d10353d48247fbd9c82f4072a24a591a790b9c
HtunHtunMyat <htun.htunmyat@seattleconsultingmyanmar.com>
2021-10-25 22:15:29 +0630

    Section 3, Ep 16 | A Few Tweaks and Consideration

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,10 @@ use App\Models\Post;
 
 Route::get('/', function () {
     $posts = Post::all();
-
     return view('posts', ['posts' => $posts]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $post = Post::find($slug);
-    
+    $post = Post::findOrFail($slug);
     return view('post', ['post' => $post]);
-})->where('post', '[A-z_\-]+');
+});
