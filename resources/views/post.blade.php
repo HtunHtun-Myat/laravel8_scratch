067662c566cac4e7563b499b073586bb350cd44c
HtunHtunMyat <htun.htunmyat@seattleconsultingmyanmar.com>
2021-10-25 22:04:19 +0630

    Section 3, Ep 15 | Blade Layouts Two Ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
+<x-layout>
     <article>
         <h1>
             {{ $post->title }}
@@ -18,7 +7,4 @@
             {!! $post->body !!}
         </div>
     </article>
-</body>
-<script src="/app.js"></script>
-
-</html>
+</x-layout>
