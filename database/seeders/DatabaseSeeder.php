1d7fca27dcfe8b77e2da0bf6d6282fe6a874be72
HtunHtunMyat <htun.htunmyat@seattleconsultingmyanmar.com>
2021-10-27 21:38:01 +0630

    Section 4, Ep 28 | Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Posts;
@@ -16,54 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Posts::truncate();
+        $user = User::factory()->create(['name' => "Mr. John"]);
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => "Personal",
-            'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-            'name' => "Family",
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => "Work",
-            'slug' => 'work'
-        ]);
-
-        Posts::create([
-            'category_id' => $personal->id,
-            'user_id' => $user->id,
-            'title' => "My Personal Post",
-            'slug' => 'personal-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error assumenda doloribus quod laboriosam expedita eaque accusantium dolor eum, quasi, officiis voluptate dicta qui quisquam vel veniam illo ab voluptas eos?</p>'
-        ]);
-
-        Posts::create([
-            'category_id' => $family->id,
-            'user_id' => $user->id,
-            'title' => "My Family Post",
-            'slug' => 'family-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error assumenda doloribus quod laboriosam expedita eaque accusantium dolor eum, quasi, officiis voluptate dicta qui quisquam vel veniam illo ab voluptas eos?</p>'
-        ]);
-
-        Posts::create([
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-            'title' => "My Work Post",
-            'slug' => 'work-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error assumenda doloribus quod laboriosam expedita eaque accusantium dolor eum, quasi, officiis voluptate dicta qui quisquam vel veniam illo ab voluptas eos?</p>'
-        ]);
-
-
+        Posts::factory(5)->create(['user_id' => $user->id]);
     }
 }
